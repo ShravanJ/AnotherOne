4f083cb5967def8f6f01ac7fe04fff780cbf5a3a
Shravan Jambukesan <shravan.j97@gmail.com>
2015-12-09 17:31:18 -0600

    Initial Commit
    First commit

---
+++ AnotherOneTester.java
@@ -0,0 +1,15 @@
+/**
+ * The AnotherOneTester class demonstrates the functionality of the AnotherOne class
+ * @author Shravan Jambukesan
+ * Date: 12/9/15
+ * https://github.com/ShravanJ/AnotherOne
+ */
+
+
+public class AnotherOneTester
+{
+	public static void main(String[] args)
+	{
+		AnotherOne DJKhaled = new AnotherOne();
+	}
+}
